48ce47921aae56f1626f97c66fa7e59e3b5963b0
Emilio Quiambao <emilioquiambao@gmail.com>
2019-05-21 16:14:57 -0700

    fixed circle nav, adjusted particles

--- static/p5/lines-proj/lines.js
+++ static/p5/lines-proj/lines.js
@@ -1,6 +1,7 @@
 var num_lines = 15;
 var t = 0;
 var op = 55;
+var mouse = 0;
 
 function setup() {
   //background(20);
@@ -8,7 +9,6 @@ function setup() {
 }
 
 function draw() {
-  //ackground(20);
   clear();
   stroke(250,230,200);
   strokeWeight(5);
@@ -18,10 +18,30 @@ function draw() {
   if(op > 55) {
     op -= 5;
   }
+
+  if(mouse > 0) {
+    mouse -= 1;
+  }
+
+
+
+  if(mouseX > 45 && mouseX < 255 && mouseY > 45 && mouseY < 255) {
+    op+=25;
+    mouse+=5;
+    t-=.20;
+  
+    if(op >= 125) {
+      op = 125;
+    }
+  
+    if(mouse >= 125) {
+      mouse = 125;
+    }
+  }
+
+  console.log(mouseX, mouseY);
   
   for(let i = 0; i < num_lines; i++) {
-    console.log(winMouseX, winMouseY);
-
     /*
     //BLUE
     stroke(250 - (i * 10), 230, 200, 55);
@@ -35,6 +55,7 @@ function draw() {
     */
 
 
+    /*
     //BLUE
     stroke(250 - (i * 10), 230, 200, op);
     line(x1(t+i), y1(t+i), x2(t+i), y2(t+i));
@@ -43,22 +64,36 @@ function draw() {
     line(x2(t+i) - mouseY, y1(t-i) * mouseX  + mouseX, x1(t+i) + mouseY, y2(t-i)  * mouseX);
     //YELLOW
     stroke(250, 230, 200 - (i * 15), op);
-    line(x2(t-i) + mouseX, x1(t-i), y1(t-i), y2(t+i)  + mouseX);
+    line(x2(t-i) + mouseX, x1(t-i), y1(t-i), y2(t+i)  + mouseX); */
 
+    //BLUE
+    stroke(250 - (i * 10), 230, 200, op);
+    line(x1(t+i) + mouseY, y1(t+i) - mouse, x2(t+i), y2(t+i) + mouseY);
+    //PURPLE
+    stroke(250, 230 - (i * 5), 200, op);
+    line(x2(t+i) - mouseY, y1(t-i) + mouse, x1(t+i) + mouseX, y2(t-i)  + mouse);
+    //YELLOW
+    stroke(250, 230, 200 - (i * 15), op);
+    line(x2(t-i) + mouse, x1(t-i) + mouseX, y1(t-i) + mouseY, y2(t+i)  + mouse);
   }
-
-  //point(x1(t), y1(t));
-  //point(x2(t), y2(t));
+  
   t += .25;
-
 }
 
+/*
 function mouseMoved() {
-  op+=25
+  op+=25;
+  mouse+=5;
+  t-=.20;
+
   if(op >= 125) {
     op = 125;
   }
-}
+
+  if(mouse >= 125) {
+    mouse = 125;
+  }
+} */
 
 function x1(t) {
   return Math.sin(t/10) * 100 + Math.sin(t/15) * 100;
--- static/p5/particle-proj/sketch.js
+++ static/p5/particle-proj/sketch.js
@@ -1,5 +1,5 @@
 const universe = [];
-var friction = .80;
+var friction = .80; //.80
 
 // pos, vel, f, rgb, att, rep, minR, maxR, damp
 
@@ -8,42 +8,60 @@ function setup() {
 
 
   //RED PARTICLES
-  for(let i = 0; i < 170; i++) {
+  for(let i = 0; i < random(25, 170); i++) {
     universe.push(new Particle(
       createVector(random(0, width - 50), random(0, height - 50)), //position
       createVector(0, 0),                 //velocity
-      0.15,//2,                                  //force
-      1,                                  //color
-      [1,3,4],                             //attracted to
-      [2,5],                                 //repelled to
+      0.04,//2,                                  //force 0.15
+      1,                                  //color 1
+      [1,3,4],                             //attracted to 134
+      [],                                 //repelled to 25
       [],                                   //passive to
-      25,                                 //minimum radius
-      70,                     //maximum radius
-      friction,                                //dampening or friction
+      25,                                 //minimum radius 25
+      65,                     //maximum radius 65
+      random(),                                //dampening or friction
       12,                                  //size
-      [random(1,5),random(1,5),random(1,5),random(1,5),random(1,5)]
+      [0.9, 0, 1.5, 3, 0]
       ));
   }
 
   //BLUE PARTICLES
-  for(let i = 0; i < 200; i++) {
+  for(let i = 0; i < random(50,200); i++) {
     universe.push(new Particle(
       createVector(random(width/4, width * (3/4)), random(height/4, height * (3/4))), //position
       createVector(0, 0),                 //velocity
       0.09,//5,                                 //force
-      3,                                  //color
-      [1,3],                                //attracted to
-      [2,4,5],                                 //repelled to
+      3,                                  //color 3 
+      [1,3],                                //attracted to 13
+      [4],                                 //repelled to 245
       [],                                   //passive to
       25,                                 //minimum radius
-      57,                                //maximum radius
-      friction,                                 //dampening or friction
+      50,                                //maximum radius 50
+      random(),                                 //dampening or friction
       12,                                  //size
-      [random(1,5),random(1,5),random(1,5),random(1,5),random(1,5)]
+      [1.7, 0, 1.3 , random(5,9) ,0]
       ));
   }
 
-  
+  //YELLOW PARTICLES
+  for(let i = 0; i < random(5,55); i++) {
+    universe.push(new Particle(
+      createVector(random(width/3, width - 200), random(height/3, height - 200)), //position
+      createVector(0, 0),                 //velocity
+      0.002,//7,                                  //force
+      4,                                  //colors 4
+      [1],                                //attracted to 215
+      [3,4],                              //repelled to 34
+      [],                                   //passive to
+      25,                                 //minimum radius
+      77,                                 //maximum radius 77
+      random(),                                //dampening or friction
+      12,                                  //size
+      [0.9, 0, 0.9, 0.9, 0]
+      ));
+  }
+
+  /*
   //GREEN PARTICLES
   for(let i = 0; i < 150; i++) {
     universe.push(new Particle(
@@ -55,13 +73,14 @@ function setup() {
       [1,4,5],                              //repelled to
       [],                                   //passive to
       25,                                 //minimum radius
-      70,                                 //maximum radius
+      77,                                 //maximum radius
       friction,                                //dampening or friction
       12,                                  //size
       [random(1,5),random(1,5),random(1,5),random(1,5),random(1,5)]
       ));
   }
-
+  */
+  /*
   //YELLOW PARTICLES
   for(let i = 0; i < 88; i++) {
     universe.push(new Particle(
@@ -78,8 +97,9 @@ function setup() {
       12,                                  //size
       [random(1,5),random(1,5),random(1,5),random(1,5),random(1,5)]
       ));
-  }
+  }*/
 
+  /*
   //PINK PARTICLES
   for(let i = 0; i < 66; i++) {
     universe.push(new Particle(
@@ -97,7 +117,7 @@ function setup() {
       [random(1,5),random(1,5),random(1,5),random(1,5),random(1,5)]
       ));
   }
-  
+  */
 }
 
 function draw() {
