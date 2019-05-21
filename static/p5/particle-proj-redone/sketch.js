6e98f40873aba1beec096bb97b9856a9b38a36b1
Emilio Quiambao <emilioquiambao@gmail.com>
2019-05-21 00:08:16 -0700

    fade in animations

---
+++ static/p5/particle-proj-redone/sketch.js
@@ -0,0 +1,18 @@
+const universe = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  background(15);
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
