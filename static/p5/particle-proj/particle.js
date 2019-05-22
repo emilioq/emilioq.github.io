fa76d8210145a0c52a504b79fc51cf6f84286118
Emilio Quiambao <emilioquiambao@gmail.com>
2019-05-21 17:00:56 -0700

    adjusted particle wall collision

--- static/p5/particle-proj/particle.js
+++ static/p5/particle-proj/particle.js
@@ -29,18 +29,22 @@ class Particle {
         //BORDERS
         if(this.position.x < 0) {
             this.velocity.x *= -1;
+            this.position.x = 0;
         }
 
         if(this.position.x > width) {
             this.velocity.x *= -1;
+            this.position.x = width;
         }
 
         if(this.position.y < 0) {
             this.velocity.y *= -1;
+            this.position.y = 0;
         }
 
         if(this.position.y > height) {
             this.velocity.y *= -1;
+            this.position.y = height;
         }
         
 
