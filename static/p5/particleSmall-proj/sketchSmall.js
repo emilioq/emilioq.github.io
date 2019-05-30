467596f3f9794a0084cfc2f25ae923f47e03a83e
Emilio Quiambao <emilioquiambao@gmail.com>
2019-05-29 22:18:05 -0700

    added 4th particle and adjusted forces

--- static/p5/particleSmall-proj/sketchSmall.js
+++ static/p5/particleSmall-proj/sketchSmall.js
@@ -14,14 +14,14 @@ function setup() {
       0.04,//2,                                  //force 0.15
       1,                                  //color 1
       [1,3],                             //attracted to 134
-      [2],                                 //repelled to 25
+      [2,4],                                 //repelled to 25
       [],                                   //passive to
-      [25, 35, 25],                                 //minimum radius 25
-      [65, 70, 55],                     //maximum radius 65
+      [15, 35, 25, 20],                                 //minimum radius 25
+      [30, 70, 65, 60],                     //maximum radius 65
       random(0.2,0.95),                                //dampening or friction
       random(3,9),                                  //size
-      [0.9, 2, 2]
-      ));
+      [0.9, 2, 2, 3]
+    ));
   }
 
   //BLUE PARTICLES (200)
@@ -32,14 +32,14 @@ function setup() {
       0.09,//5,                                 //force
       2,                                  //color 3 
       [1,2],                                //attracted to 13
-      [3],                                 //repelled to 245
+      [3,4],                                 //repelled to 245
       [],                                   //passive to
-      [10, 13, 10],                                 //minimum radius
-      [55, 25, 40],                                //maximum radius 50 ... 55
+      [10, 13, 10, 25],                                 //minimum radius
+      [55, 25, 60, 65],                                //maximum radius 50 ... 55
       random(0.2,0.95),                                 //dampening or friction
       random(3,9),                                  //size
-      [1.9, 1, 7]
-      ));
+      [1.9, 1, 7, 3]
+    ));
   }
 
   //YELLOW PARTICLES (33)
@@ -50,15 +50,33 @@ function setup() {
       0.1,//7,                                  //force
       3,                                  //colors 4
       [1,2],                                //attracted to 215
-      [3],                              //repelled to 34
+      [3,4],                              //repelled to 34
       [],                                   //passive to
-      [25, 15, 25],                                 //minimum radius
-      [77, 50, 70],                                 //maximum radius 77
+      [15, 15, 30, 25],                                 //minimum radius
+      [77, 55, 90, 70],                                 //maximum radius 77
       random(0.2,0.95),                                //dampening or friction
       random(3,9),                                  //size
-      [0.75, 0.9, 1.2]
-      ));
+      [1.0, 0.9, 1.2, 3]
+    ));
   }
+
+    //GREEN PARTICLES (33)
+    for(let i = 0; i < 5; i++) {
+      universe.push(new Particle(
+        createVector(random(width/4, width * (3/4)), random(height/4, height * (3/4))), //position
+        createVector(0, 0),                 //velocity
+        0.2,//7,                                  //force
+        4,                                  //colors 4
+        [1,2,3],                                //attracted to 215
+        [4],                              //repelled to 34
+        [],                                   //passive to
+        [15, 12, 10, 30],                                 //minimum radius
+        [50, 45, 50, 65],                                 //maximum radius 77
+        random(0.2,0.95),                                //dampening or friction
+        random(3,9),                                  //size
+        [0.5, 0.5, 0.9, 2.6]
+      ));
+    }
 }
 
 function draw() {
@@ -73,7 +91,7 @@ function draw() {
 }
 
 function mousePressed() {
-  let p = int(random(1,4));
+  let p = int(random(1,5));
   switch(p) {
     case 1:
       universe.push(new Particle(
@@ -82,13 +100,13 @@ function mousePressed() {
         0.04,//2,                                  //force 0.15
         1,                                  //color 1
         [1,3],                             //attracted to 134
-        [2],                                 //repelled to 25
+        [2,4],                                 //repelled to 25
         [],                                   //passive to
-        [25, 35, 25],                                 //minimum radius 25
-        [65, 70, 55],                     //maximum radius 65
+        [15, 35, 25, 20],                                 //minimum radius 25
+        [30, 70, 65, 60],                     //maximum radius 65
         random(0.2,0.95),                                //dampening or friction
         random(3,9),                                  //size
-        [0.9, 2, 2]
+        [0.9, 2, 2, 3]
       ));
       break;
     case 2:
@@ -98,13 +116,13 @@ function mousePressed() {
         0.09,//5,                                 //force
         2,                                  //color 3 
         [1,2],                                //attracted to 13
-        [3],                                 //repelled to 245
+        [3,4],                                 //repelled to 245
         [],                                   //passive to
-        [10, 13, 10],                                 //minimum radius
-        [55, 25, 40],                                //maximum radius 50
+        [10, 13, 10, 25],                                 //minimum radius
+        [55, 25, 60, 65],                                //maximum radius 50
         random(0.2,0.95),                                 //dampening or friction
         random(3,9),                                  //size
-        [1.9, 1, 7]
+        [1.9, 1, 7, 3]
       ));
       break;
     case 3:
@@ -114,15 +132,32 @@ function mousePressed() {
         0.1,//7,                                  //force
         3,                                  //colors 4
         [1,2],                                //attracted to 215
-        [3],                              //repelled to 34
+        [3,4],                              //repelled to 34
         [],                                   //passive to
-        [25, 15, 25],                                 //minimum radius
-        [77, 50, 70],                                 //maximum radius 77
+        [15, 15, 30, 25],                                 //minimum radius
+        [77, 55, 90, 70],                                 //maximum radius 77
         random(0.2,0.95),                                //dampening or friction
         random(3,9),                                  //size
-        [0.75, 0.9, 1.2]
+        [1.0, 0.9, 1.2, 3]
       ));
       break;
+    case 4:
+      universe.push(new Particle(
+        createVector(mouseX, mouseY), //position
+        createVector(0, 0),                 //velocity
+        0.2,//7,                                  //force
+        4,                                  //colors 4
+        [1,2,3],                                //attracted to 215
+        [4],                              //repelled to 34
+        [],                                   //passive to
+        [15, 12, 10, 30],                                 //minimum radius
+        [50, 45, 50, 65],                                 //maximum radius 77
+        random(0.2,0.95),                                //dampening or friction
+        random(3,9),                                  //size
+        [0.5, 0.5, 0.9, 2.6]
+      ));
+      break;
+        
   }
 }
 
